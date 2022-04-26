ab8543b814f80c39a1dc50ed224d771c9b991f58
HCW <1547954274@qq.com>
2022-04-26 13:34:25 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,5 @@
+from time import time
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 import unittest
 
@@ -15,7 +17,34 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists.
-        self.assertIn('To-Do', self.browser.title, "Browser title was" + self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # She types "Buy peacock feathers" into a text box.
+        # (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers to make a fly".(Edith is very methodical)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        #There is still a text box inviting her to add another item. She
+        #enters "Use peacock feathers to make a fly" (Edith is very methodical)
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
@@ -42,7 +71,7 @@ if __name__ == '__main__':
 
 # Edith wonders whether the site will remember her list.
 # Then she sees that the site has generated a unique URL for her
-# -- there is some explanatory ttext to that effect.
+# -- there is some explanatory text to that effect.
 
 # She visits tahr URL - her to-do list is still there.
 
