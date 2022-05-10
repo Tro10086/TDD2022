c74dab7669302f5a4c24db89630e41dfb70fd3f7
HCW <1547954274@qq.com>
2022-05-10 13:14:48 +0800

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,7 @@
-from cgitb import html
-from urllib import request, response
+from urllib import response
 from django.test import TestCase
 from django.urls import resolve
-from django.http import HttpRequest
+
 from lists.views import home_page
 
 # Create your tests here.
@@ -13,14 +12,16 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolve_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    # def test_home_page_returns_correct_html(self):
+    #     response = self.client.get('/')
+
+    #     html = response.content.decode('utf8')
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.strip().endswith('</html>'))
+        
+    #     self.assertTemplateUsed(response, 'home.html')
